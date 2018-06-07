7cb12f35e89335a154e9f13de687fc9e03804573
BusinessL <1138326692@qq.com>
2018-06-07 16:51:39 +0800

    水仙花数

--- app/Http/Controllers/MathsController.php
+++ app/Http/Controllers/MathsController.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace App\Http\Controllers;
 /**
 * Author:LQ
 * About Math Arithmetic  
@@ -7,9 +8,9 @@
 class MathsController extends Controller
 {
 	
-	function __construct(argument)
+	public function __construct()
 	{
-		# code...
+		// $this->middleware('auth');
 	}
 
     /**
@@ -41,5 +42,36 @@ class MathsController extends Controller
             }
         }   
       return $result;
+    }
+    /**
+     * 水仙花数.
+     *
+     *题目描述：春天是鲜花的季节，水仙花就是其中最迷人的代表，数学上有个水仙花数，他是这样定义的： 
+     *“水仙花数”是指一个三位数，它的各位数字的立方和等于其本身，比如：153=1^3+5^3+3^3。  *现在要求输出所有在m和n范围内的水仙花数。
+     *
+     *输入：输入数据有多组，每组占一行，包括两个整数m和n（100 ≤ m ≤ n ≤ 999）。
+     *
+     *输出：对于每个测试实例，要求输出所有在给定范围内的水仙花数，就是说，输出的水仙花数必须大于*等于m,并且小于等于n，如果 *有多个，则要求从小到大排列在一行内输出，之间用一个空格隔开;
+	 *如果给定的范围内不存在水仙花数，则输出no;
+	 *每个测试实例的输出占一行。
+     * @param  int   $n
+     * @param  int   $m  
+     * @return array
+     */
+    public function narcissisticNumber(){
+    	fscanf(STDIN,'%d %d',$a,$b);
+    	$n = 100;
+    	$m = 999;
+    	$result = [];
+    	for($n;$n<=$m;$n++){
+    		$a = intval($n/100);
+    		$b = intval($n/10) % 10;
+    		$c = $n %10;
+
+    		if(pow($a,3) + pow($b,3) + pow($c,3) == $n){
+    			array_push($result, $n);
+    		}
+    	}
+    	return $result;
     }
 }
